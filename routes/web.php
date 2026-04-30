2962d6162e1da1fed0504bb96c4f0a1e735b2727
Vinicius Oliveira <vinsouzza@gmail.com>
2026-04-30 16:22:44 -0300

    007 - limpando o projeto

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,3 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo "Hello world";
-//return view('welcome');
-});
-
-Route::get('/teste', function(){
-    echo "Teste haha";
-});
-
-Route::get('/main/{value}', [MainController::class, 'index']);
-Route::get('/page2/{value}', [MainController::class, 'page2']);
-Route::get('/page3/{value}', [MainController::class, 'page3']);
