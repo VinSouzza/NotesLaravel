a6a2ad2f53d305c7d811ff4becd0fabea238c487
Vinicius Oliveira <vinsouzza@gmail.com>
2026-05-08 15:05:26 -0300

    015 - adicionando informações de conexão no .env e testando a conexão via controller.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
@@ -32,7 +33,15 @@ class AuthController extends Controller
         $username = $request -> input('text_username');
         $password = $request -> input('text_password');
 
-        echo "Ok!";
+        // test database connection
+        try {
+            DB::connection()->getPdo();
+            echo "Connection  is Ok";
+        } catch (\PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+
+        echo "Fim";
     }
 
     public function logout(){
