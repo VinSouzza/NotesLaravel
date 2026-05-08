ca35769c8a271156eabe4ef72898ee1c7370e34b
Vinicius Oliveira <vinsouzza@gmail.com>
2026-05-08 14:28:32 -0300

    012 - Aplicando validações do laravel por parte do servidor para os campos do login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,9 +11,19 @@ class AuthController extends Controller
     }
 
     public function loginSubmit(Request $request){
-        echo $request ->input('text_username');
-        echo '<br>';
-        echo $request ->input('text_password');
+        // form validation 
+        $request->validate(
+            [
+                'text_username' => 'required',
+                'text_password' => 'required'
+            ]
+        );
+
+        // get users input
+        $username = $request -> input('text_username');
+        $password = $request -> input('text_password');
+
+        echo "Ok!";
     }
 
     public function logout(){
