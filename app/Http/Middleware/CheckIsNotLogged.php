9b291c98b801dbe909c88736ae0e2554b8fa0a42
Vinicius Oliveira <vinsouzza@gmail.com>
2026-05-11 11:51:38 -0300

    021 - adicionando um segundo middleware que verifica se o usuário NÃO esta logado, e redireciona para o index

---
+++ app/Http/Middleware/CheckIsNotLogged.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckIsNotLogged
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // verificando se o usuário NÃO esta logado
+        if(session('user')){
+            return redirect('/');
+        }
+        return $next($request);
+    }
+}
