b036cce1ad03fc75cd9738e9717258ff786220cf
Vinicius Oliveira <vinsouzza@gmail.com>
2026-05-11 11:47:06 -0300

    021 - adicionando um middleware que verifica se o usuário esta logado e, caso não, não permite acessar as demais rotas do sistema.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,4 +6,10 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
+    public function index(){
+        echo 'Eu to aqui paezão!';
+    }
+    public function newNote(){
+        echo 'Eu to criando uma nova nota';
+    }
 }
