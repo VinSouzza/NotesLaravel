36521ec8b6f140d63e26bb28e4fa6ed9693cde6f
Vinicius Oliveira <vinsouzza@gmail.com>
2026-05-12 16:59:48 -0300

    031 - adicionando um serviço de desencriptação global onde seu metodo estatico pode ser chamado em qualquer classe.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
@@ -26,27 +27,15 @@ class MainController extends Controller
 
     public function editNote($id){
         
-        $id = $this->decryptId($id);
-        
+        $id = Operations::decryptId($id);
         echo "estou editando a nota com id = $id";
     }
 
     public function deleteNote($id){
         
-        $id = $this->decryptId($id);
-        
+        $id = Operations::decryptId($id);
         echo "estou deletando a nota com id = $id";
     }
 
-    private function decryptId($id){
 
-        // se o id estiver encriptado 
-        try {
-            $id = Crypt::decrypt($id);
-        } catch (DecryptException $e) {
-            return redirect()->route('home');
-        }
-
-        return $id;
-    }
 }
