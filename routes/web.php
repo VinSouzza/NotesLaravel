a328fff2d1fd011da0444b2f9f7dfdaeb79dbe07
Vinicius Oliveira <vinsouzza@gmail.com>
2026-05-14 14:23:18 -0300

    035 - adicionando validação e função funcionando de update da nota

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::middleware([CheckIsLogged::class])->group(function(){
     
     //edit note
     Route::get('/edit/{id}', [MainController::class, 'editNote'])->name('edit');
-    Route::get('/editNoteSubmit', [MainController::class, 'editNoteSubmit'])->name('editNoteSubmit');
+    Route::post('/editNoteSubmit', [MainController::class, 'editNoteSubmit'])->name('editNoteSubmit');
     
     //edit note
     Route::get('/delete/{id}', [MainController::class, 'deleteNote'])->name('delete');
