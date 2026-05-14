7fb7728f550b6b745223703bcedff4ed10fb308f
Vinicius Oliveira <vinsouzza@gmail.com>
2026-05-14 11:52:43 -0300

    033 - adicionando verificação e funcionalidade de criar nova nota na MainController e na View new_note.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use App\Models\User;
 use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
@@ -27,7 +28,39 @@ class MainController extends Controller
     }
 
     public function newNoteSubmit(Request $request){
-        echo "Estou criando uma nova nota";
+        // validate request
+        $request->validate(
+            // rules
+            [
+                'text_title' => ['required','min:3','max:200'],
+                'text_note' => 'required|min:3|max:3000'
+            ],
+            // error messages
+            [
+                'text_title.required' => 'O título é obrigatório',
+                'text_title.min' => 'O título deve ter no minimo :min caracteres',
+                'text_title.max' => 'O título deve ter no máximo :max caracteres',
+
+                'text_note.required' => 'A nota é obrigatório',
+                'text_note.min' => 'A nota deve ter no minimo :min caracteres',
+                'text_note.max' => 'A nota deve ter no máximo :max caracteres',
+            ]
+        );
+
+        // get user id
+        $id = session('user.id');
+
+        // create new note
+        $note = new Note();
+        $note->user_id = $id;
+        $note->title = $request->text_title;
+        $note->text = $request->text_text;
+
+        $note->save();
+
+        // redirect to home.
+        return redirect(route('home'));
+
     }
 
     public function editNote($id){
